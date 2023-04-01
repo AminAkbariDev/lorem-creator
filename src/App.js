aec7a6484d22316ced086d49312c1c5ead0158fd
AminAkbariDev <aminakbari.dev@gmail.com>
2023-04-01 03:37:39 +0330

    Deploy website

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import data from "./data";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 0;
+    } else if (count > 4) {
+      amount = 4;
+    }
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <div className="section-center">
+      <h4>لورم ساز خوشگل و زیبا</h4>
+      <form className="lorem" onSubmit={handleSubmit}>
+        <label htmlFor="total">تعداد پاراگراف:</label>
+        <input
+          type="number"
+          name="total"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit">بساز</button>
+      </form>
+
+      <article className="lorem-text">
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </div>
+  );
+}
+
+export default App;
